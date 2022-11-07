6e08a2b95c650873334d71b9bea971dfe649cb53
Samuel Cordova <cordovas1337@gmail.com>
2022-11-07 09:13:13 -0600

    initial commit and starter code

---
+++ script.js
@@ -0,0 +1,9 @@
+// Try to create a maze generation / traversal project using Depth-First search algo.
+
+/* Depth first search
+    1. Given current node as a parameter
+    2. Mark current node as visited
+    3. For every edge from the current node -> new node N
+        a. If the new node N has not been visited
+        b. Invoke this routine recursively for the new node N
+*/
