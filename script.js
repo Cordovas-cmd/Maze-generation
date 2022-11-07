a891eba2c48d117740ccfac6e44c0cc593127549
Samuel Cordova <cordovas1337@gmail.com>
2022-11-07 09:16:55 -0600

    added more psuedo code

--- script.js
+++ script.js
@@ -7,3 +7,12 @@
         a. If the new node N has not been visited
         b. Invoke this routine recursively for the new node N
 */
+
+/* Recursive routine for depth-first search 
+    1. Given a current cell as a parameter
+    2. Mark the current cell as visited
+    3. WHile the current cell has any unvisited neighbour cells
+        a. CHose one of the unvisited neighbours
+        b. Remove the wall between the current cell and the chosen cell
+        c. Invoke the routine recursively for a chosen cell.
+        which is invoked once for any initial cell in the area.*/
